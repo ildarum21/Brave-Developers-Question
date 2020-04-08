a74e471fba512bec5722b342b2e19144227544cb
ildarum21 <61203680+ildarum21@users.noreply.github.com>
2020-04-09 00:50:26 +0400

    readme,docker

--- SecQuestion/server.js
+++ SecQuestion/server.js
@@ -10,6 +10,7 @@ app.set('view engine','ejs');
 app.use('/public',express.static('public'))
 
 
+
 app.get('/',function (req,res) {
     res.render('index',{text:" Выбери оператора и нажми оплатить"});
  });
